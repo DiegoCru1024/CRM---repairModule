ca7bc075a7e7ca03ddf0e081bba018dc05037e81
MeekoDev <DiegoCru1024@hotmail.com>
2023-09-23 23:08:17 -0500

    Avance de Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import LoginPage from "./components/loginPage/loginPage";
+import DashboardPage from "./components/dashboardPage/dashboardPage";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path={'/'} element={<LoginPage/>}/>
+                <Route path={'/dashboard'} element={<DashboardPage/>}/>
             </Routes>
         </BrowserRouter>
     );
---
+++ frontend/src/components/dashboardPage/dashboardPage.js
@@ -0,0 +1,14 @@
+import SideBar from "../sideBarComponent/sideBar";
+import styles from './dashboardPage.module.css'
+
+export default function DashboardPage() {
+    return (
+        <div className={styles.mainContainer}>
+            <SideBar/>
+
+            <div className={styles.dashboardContainer}>
+                <h1>DashBoard</h1>
+            </div>
+        </div>
+    )
+}

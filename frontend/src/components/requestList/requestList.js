755236a808b0c48cb7c5052a573c00db1eba7afb
MeekoDev <DiegoCru1024@hotmail.com>
2023-10-11 18:35:27 -0500

    Lista actualizada

--- frontend/src/components/requestList/requestList.js
+++ frontend/src/components/requestList/requestList.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import SideBar from '../sideBarComponent/sideBar';
 import styles from './requestList.module.css';
+import ListComponent from "../listComponent/listComponent";
 
 
 export default function RequestList() {
+    const listData = [
+        {
+            id: 123,
+            date: '12/10/2023',
+            name: 'Diego Cruces',
+            technicianID: 455,
+            details: 'Ninguno'
+
+        }   
+    ]
     return (
         <div className={styles.mainContainer}>
-            <SideBar />
+            <SideBar/>
             <div className={styles.requestListContainer}>
                 <div className={styles.listContainer}>
                     <h2>Lista de solicitudes emitidas</h2>
@@ -15,46 +26,7 @@ export default function RequestList() {
                     <div className={'cuadro'}>
                         <div className={'busqueda'}></div>
                         <div className={'barra-filtros'}></div>
-                        <div className={styles.table}>
-                            <table >
-                                <thead>
-                                    <tr>
-                                        <th>ID</th>
-                                        <th>FECHA</th>
-                                        <th>CLIENTE</th>
-                                        <th>TECNICO</th>
-                                        <th>DETALLES</th>
-                                        <th>ESTADO</th>
-                                        <th>ACCION</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>#11223344L</td>
-                                        <td>2023-10-15</td>
-                                        <td>Juan Luis Guerra Seijas</td>
-                                        <td>Técnico A</td>
-                                        <td>Detalles del pedido</td>
-                                        <td>
-                                            <div className={styles.estadoButton + ' ' + styles.anaranjado}>Proceso</div>
-                                        </td>
-                                        <td><button>⋮</button></td>
-                                    </tr>
-                                    <tr>
-                                        <td>#22334455p</td>
-                                        <td>2023-10-15</td>
-                                        <td>Jose Luis Perez Albela</td>
-                                        <td>Técnico B</td>
-                                        <td>Detalles del pedido</td>
-                                        <td>
-                                            <div className={styles.estadoButton + ' ' + styles.verde}>Pendiente</div>
-                                        </td>
-                                        <td><button>⋮</button></td>
-                                    </tr>
-                                    {/* Agrega más filas según sea necesario */}
-                                </tbody>
-                            </table>
-                        </div>
+                        <ListComponent data={listData}/>
                     </div>
                 </div>
             </div>

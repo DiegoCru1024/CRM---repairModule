80b997473ff4dc135bae36023f9e565103e81872
Diego-linares1 <diego.linares1@unmsm.edu.pe>
2023-10-21 18:50:31 -0500

    cambios

--- frontend/src/components/requestList/requestList.js
+++ frontend/src/components/requestList/requestList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SideBar from '../sideBarComponent/sideBar';
 import styles from './requestList.module.css';
-
+/*import 'bootstrap/dist/css/bootstrap.min.css';*/
 
 export default function RequestList() {
     return (
@@ -51,9 +51,39 @@ export default function RequestList() {
                                         </td>
                                         <td><button>⋮</button></td>
                                     </tr>
-                                    {/* Agrega más filas según sea necesario */}
+
                                 </tbody>
                             </table>
+                            {/*<table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">First</th>
+                                        <th scope="col">Last</th>
+                                        <th scope="col">Handle</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td colspan="2">Larry the Bird</td>
+                                        <td>@twitter</td>
+                                    </tr>
+                                </tbody>
+                            </table> 
+                            */}
                         </div>
                     </div>
                 </div>

ceddc4e61bdf32e218cb688f85663382867df676
Paulo C Calderón B <paulo.napasa3@gmail.com>
2023-10-24 01:52:07 -0500

    Avance

--- frontend/src/components/repairRequestPlataform/repairRequestPlataform.js
+++ frontend/src/components/repairRequestPlataform/repairRequestPlataform.js
@@ -6,88 +6,101 @@ const RepairRequestPlataform = () => {
   return (
     <div className={styles.mainContainer}>
       <SideBar />
-      <div className={styles.bodyContainer}>
+      <div className={styles.repairRequestContainer}>
         <div className={styles.form}>
-          <h1>Generar solicitud</h1>
-
-          {/* Venta Asociada */}
-          <div className={styles.sale}>
-            <label>Venta asociada*</label>
-            <input type="text" id="venta" name="venta" required />
-          </div>
-
-          {/* Datos */}
-          <h2>Datos</h2>
-          <div className={styles.dataContainer}>
-            <FormData
-              label="Nombre y Apellidos"
-              name="nombre_apellidos"
-              readOnly
-            />
-            <FormData
-              label="Telefono de contacto"
-              name="contacto"
-              type="number"
-              readOnly
-            />
-            <FormData
-              label="Correo asociado"
-              name="email"
-              type="email"
-              readOnly
-            />
-            <FormData label="DNI" name="dni" type="number" readOnly />
-            <FormData label="Servicio/Modelo" name="servicio_modelo" readOnly />
-            <FormData label="Garantía" name="garantia" type="number" readOnly />
-          </div>
-
-          {/* Solicitud */}
-          <h2>Solicitud</h2>
-          <form action="/RepairRequest" method="post">
-            <div className={styles.request}>
-              <div>
-                <div style={{ display: "flex" }}>
-                  <FormData
-                    label="Fecha de ingreso *"
-                    name="fecha_ingreso"
-                    required
-                  />
-                  <SelectData
-                    label="Motivo *"
-                    name="ciudad"
-                    options={["pantalla", "bateria", "otro"]}
-                    required
-                  />
-                </div>
-                <SelectData
-                  label="Estado del equipo *"
-                  name="ciudad"
-                  options={["estado1", "estado2", "estado3"]}
-                  required
-                />
-                <FormData
-                  label="Correo alternativo"
-                  name="correo_alternativo"
-                  required
-                />
-                <div>
-                  <label>
-                    *Se enviará una notificación al correo asociado y/o
-                    alternativo
-                  </label>
-                </div>
-              </div>
-              <TextareaData
-                label="Descripcion *"
-                name="descripcion"
-                rows="8"
-                cols="18"
-                required
-              />
+          <div>
+            {/* Venta Asociada */}
+            <div className={styles.sale}>
+              <h1>Generar solicitud</h1>
+              <label>Venta asociada*</label>
+              <input type="text" id="venta" name="venta" required />
             </div>
 
-            <button type="submit">Enviar</button>
-          </form>
+            {/* Datos */}
+            <div style={{ display: "block" }}>
+              <h2>Datos</h2>
+              <div className={styles.dataContainer}>
+                <FormData
+                  label="Nombre y Apellidos"
+                  name="nombre_apellidos"
+                  readOnly
+                />
+                <FormData
+                  label="Telefono de contacto"
+                  name="contacto"
+                  type="number"
+                  readOnly
+                />
+                <FormData
+                  label="Correo asociado"
+                  name="email"
+                  type="email"
+                  readOnly
+                />
+                <FormData label="DNI" name="dni" type="number" readOnly />
+                <FormData
+                  label="Servicio/Modelo"
+                  name="servicio_modelo"
+                  readOnly
+                />
+                <FormData
+                  label="Garantía"
+                  name="garantia"
+                  type="number"
+                  readOnly
+                />
+              </div>
+            </div>
+            <div>
+              {/* Solicitud */}
+              <h2>Solicitud</h2>
+              <form action="/RepairRequest" method="post">
+                <div className={styles.request}>
+                  <div>
+                    <div style={{ display: "flex" }}>
+                      <FormData
+                        label="Fecha de ingreso *"
+                        name="fecha_ingreso"
+                        required
+                      />
+                      <SelectData
+                        label="Motivo *"
+                        name="ciudad"
+                        options={["pantalla", "bateria", "otro"]}
+                        required
+                      />
+                    </div>
+                    <SelectData
+                      label="Estado del equipo *"
+                      name="ciudad"
+                      options={["estado1", "estado2", "estado3"]}
+                      required
+                    />
+                    <FormData
+                      label="Correo alternativo"
+                      name="correo_alternativo"
+                      required
+                    />
+                    <div>
+                      <label>
+                        *Se enviará una notificación al correo asociado y/o
+                        alternativo
+                      </label>
+                    </div>
+                  </div>
+                  <TextareaData
+                    label="Descripcion *"
+                    name="descripcion"
+                    rows="8"
+                    cols="18"
+                    required
+                  />
+                </div>
+
+                <button type="submit">Enviar</button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -101,7 +114,7 @@ const FormData = ({
   readOnly = false,
   required = false,
 }) => (
-  <div style={{ display: "flex", flexDirection: "column"}}>
+  <div style={{ display: "flex", flexDirection: "column" }}>
     <label>{label}</label>
     <input type={type} name={name} readOnly={readOnly} required={required} />
   </div>
